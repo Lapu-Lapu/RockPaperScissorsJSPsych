9f7e896255b7e1f92b44a657f3be54a16bc38098
Benjamin Knopp <benjamin.knopp@uni-marburg.de>
2023-01-20 11:53:57 +0100

    Change saved strategy for consistency.

--- rps_game.js
+++ rps_game.js
@@ -151,8 +151,8 @@ const dont_always_copy_opponent_move = function () {
 } // https://www.kaggle.com/code/mainakchain/rps-getting-started-with-researched-winning-logic
 
 const strategies = [super_male_strategy, nash_equilibrium_strategy, winstay, rotate_strategy, learn_preference, dont_always_copy_opponent_move];
-const sampled_strategy = Math.floor(Math.random() * strategies.length - 1) + 1;
-//const sampled_strategy = 4;
+//const sampled_strategy = Math.floor(Math.random() * strategies.length - 1) + 1;
+const sampled_strategy = 4;
 console.log('strategy:');
 console.log(sampled_strategy);
 const strategy = learn_preference;
